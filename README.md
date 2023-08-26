c706e3c5cd225677953dd4ecc62ad9b294dc885c
Tobias <tobiasop@gmail.com>
2023-08-26 16:19:26 +0200

    make files for initializing and creating datasets
    Make info on how to download dataset and process (in README.md). The functions are stored in  and can be ran with . The  loads the data on a pytorch dataloader format, with transforms and other options.

--- README.md
+++ README.md
@@ -6,4 +6,22 @@ I currently work with training neural networks with concept, mainly based on the
 
 More high level documentation will come here after a while, but the code is documented as I code.
 
+## CUB dataset
+
+The repo works a lot with the Caltech-UCSD Birds-200-2011 (CUB) dataset [link](https://authors.library.caltech.edu/27452/1/CUB_200_2011.pdf). In order to get the code working, the user should download this dataset, in addition to the processed version from the CBM paper. The files can be found [here](https://worksheets.codalab.org/worksheets/0x362911581fcd4e048ddfd84f47203fd2) (download `CUB_200_2011` and `CUB_processed`). It is recommended to save these two folders inside a folder called `data/`.
+
+The processed dataset are pickled lists of dictionaries, which among other things contains the path to the images. The paths provided by the CBM paper are specific to their computer, but this can be easily adjusted by running:
+
+```terminal
+python initialize.py make_paths --base_path `your_path`
+```
+
+If one wishes to make datasets that are smaller, so that one can do rapid testing with less than the 200 classes, one can run:
+
+```terminal
+ptyhon initialize.py make_dataset --n_classes 10 --random_choice True
+````
+
+This creates lists of dictionaries of only `n_classes` of the 200 classes.
+
 ![no gif :(](https://media.giphy.com/media/mcsPU3SkKrYDdW3aAU/giphy.gif)
---
+++ dataset.py
@@ -0,0 +1,234 @@
+import pickle
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+
+class CUBDataset(Dataset):
+    def __init__(self, data_path=None, data_list=None, transform=None, mapping=None):
+        """
+        Dataset object for CUB dataset. The Dataloader returns
+        (PIL-image, class-label-int, list-of-attribute-labels, img_path).
+
+        This uses the CUB dataset and the preprocessed
+        version from the CBM 2020 paper. Note that the CBM paper have the authors full
+        path as image path arguments. This can be updated by calling `make_correct_paths()`
+        from `initialize.py`, or by `python initialize.py --base_path base_path`.
+
+        The original CUB dataset should be stored in the given `base_path`, where is `data/`
+        is used for the author. This folder should be in the same folder as this file if one
+        uses relative paths.
+        Additionally, the preprocessed pickle files from the CBM paper should also be in the
+        same folder. This pickle files contains a list of a object-level dict, with paths,
+        class labels, attribute labels and attribute certainty.
+
+        To access the CBM pickle data, please either proved `data_path` as a path to where
+        it lies, or `data_list` where the pickle files are already read.
+
+        Args:
+            data_list (list of dict): List containing dictionaries with `img_path`
+                `class_label`, and `attribute_label` keys. Should be read from
+                `CUB_processed/class_attr_data_10/train.pkl`.
+                Optionally, one can use `data_path` instead of this.
+            data_path (str): Path to where to pkl files with image-info are stored.
+                This is used if `data_list` is None.
+                Default place is "data/CUB_processed/class_attr_data_10/train.pkl",
+                which one can reach by setting `data_path` to "default_train",
+                "default_val", or "default_test".
+            transform (callable, optional): Optional transform to be applied on the image.
+                One can use `get_transforms()` to get this.
+            mapping (dict): In case the data_list use is a subset of all classes,
+                send this argument to convert the labels from bird indecies to indices in
+                0, ..., n_classes. See utils.make_small_dataset() for info of the subset,
+                this is useful for exploring since it runs much faster.
+        """
+        if data_list is not None:
+            self.data_list = data_list
+        elif data_path is not None:
+            if data_path.lower() == "default_train":
+                data_path = "data/CUB_processed/class_attr_data_10/train.pkl"
+            elif data_path.lower() == "default_val":
+                data_path = "data/CUB_processed/class_attr_data_10/val.pkl"
+            elif data_path.lower() == "default_test":
+                data_path = "data/CUB_processed/class_attr_data_10/test.pkl"
+            try:
+                data_list = pickle.load(open(data_path, "rb"))
+            except Exception as e:
+                message = f"Could not load file in path {data_path}. "
+                message += "To resolve this issue, please download the CUB dataset "
+                message += "and run `python initialize.py make_paths --base_path data/` "
+                message += "to set the correct paths. See README.md for more information."
+                raise FileNotFoundError(message)
+        else:
+            message += "Either `data_list` or `data_path` must be sent to constructor."
+            raise ValueError(message)
+
+        self.data_list = data_list
+        self.transform = transform
+        if mapping is not None:
+            self.mapping = mapping
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        img_path = self.data_list[idx]["img_path"]
+        class_label = self.data_list[idx]["class_label"]
+        attribute_label = torch.tensor(self.data_list[idx]["attribute_label"]).to(torch.float32)
+
+        image = Image.open(img_path).convert("RGB")  # Some images are grayscale
+
+        if self.transform:
+            image = self.transform(image)
+
+        if self.mapping is not None:  # Map bird index to label
+            class_label = self.mapping[class_label]
+
+        return image, class_label, attribute_label, img_path
+
+
+def get_transforms(mode, resol=224, normalization="[-1, 1]",
+                   brightness=32 / 255, saturation=[0.5, 1.5]):
+    """
+    Returns the callable for transforming the images for the CUB dataset.
+    The `normalization` argument either scales to the interval [-1, 1], or
+    uses the imagenet standard normalization values. `resol` is the height
+    and width expected by the model, which is 224 for MobileNet and ResNet.
+
+    Naturally, the train transforms does random cropping and flipping
+    (and maybe jittering), while the validation and test transforms makes a
+    deterministic center crop. The image is resized first to make sure the center
+    is not very zoomed in.
+
+    The CBM paper 2020 uses 0.5 for std instead of 2, scaling to [-0.25, 0.25].
+    It also does not resize the validation transform before centercropping.
+
+    Args:
+        mode (str): Either `train` or `val`, to indicate which transform to use.
+        resol (int, optional): The expected input height and width by the model,
+            which the transfroms transforms to. Defaults to 224.
+        normalization (str, optional): The normalization method. Defaults to "[-1, 1]".
+        brigthness (float): The brigthness to randomly increase or decreause the
+            training image with, in jittering. Use 0 for no jitter.
+        saturation (list): The interval of possible saturations to randomly jitter
+            image to. Use [1, 1] for no saturation.
+
+    Returns:
+        callable: The transforms callable.
+    """
+    assert mode.lower() in ["train", "val", "test"]
+    if normalization in ["[-1, 1]", "-1, 1", "[-1,1]", "-1,1"]:
+        normalize = torchvision.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[2, 2, 2])
+    elif normalization.lower() == "imagenet":
+        normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                     std=[0.229, 0.224, 0.225])
+
+    if mode.lower() == "train":
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.ColorJitter(brightness=brightness, saturation=saturation),
+            torchvision.transforms.RandomResizedCrop(resol),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.ToTensor(),  # Turns image into [0, 1] float tensor
+            normalize,
+        ])
+    elif mode.lower() in ["val", "test"]:
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(256),
+            torchvision.transforms.CenterCrop(resol),
+            torchvision.transforms.ToTensor(),
+            normalize,
+        ])
+    return transform
+
+
+def make_dataloader(dataset, sampler=None, batch_size=4, shuffle=True, drop_last=False):
+    """
+    Converts a Dataset to a Dataloader.
+    If one uses a costum sampler, send this as `sampler`.
+
+    Args:
+        dataset (Dataset): The Dataset to use
+        sampler (callable, optional): Costum sampler. If None, will use standard
+            sampler. Defaults to None.
+        batch_size (int, optional): Batch size to use. Defaults to 4.
+        shuffle (bool, optional): Determines wether the sampler will shuffle or not.
+            It is recommended to use `True` for training and `False` for validating.
+        drop_last (bool, optional): Determines wether the last iteration of an epoch
+            is dropped when the amount of elements is less than `batch_size`.
+
+    Returns:
+        Dataloader: The Dataloader
+    """
+    if sampler is not None:
+        batch_sampler = torch.utils.data.BatchSampler(sampler(dataset), batch_size=batch_size,
+                                                      shuffle=shuffle, drop_last=drop_last)
+        dataloader = DataLoader(dataset, sampler=batch_sampler)
+    else:
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+    return dataloader
+
+
+def load_data(mode="all", subset=None, path="data/CUB_processed/class_attr_data_10/",
+              resol=224, normalization="[-1, 1]", brightness=32 / 255,
+              saturation=[0.5, 1.5], sampler=None, batch_size=4, shuffle=True, drop_last=False):
+    """
+    Main function to call for loading the CUB dataset.
+
+    Function that calls CUBDataset, get_transforms() and make_dataloader().
+    This is meant as a convenient way of just calling one function to get the
+    final dataloaders. Takes in all of the arguments that the other functions does.
+    Note that `mode` can be in ["train", "val", "test", "all"]. If it is one of the
+    three first, this function returns only the dataloader corresponding to that set,
+    and if it is "all" then it returns a list of train, val, test loaders.
+
+    Args:
+        mode (str, optional): The datasetmode to loader. If "all", will return a
+            tuple of (train, val, test). Defaults to "all".
+        subset (int, optional): If not None, will only use a subset of the classes from the
+            CUB dataset. Note that `utils.make_small_dataset()` should be called with
+            the `n_classes` corresponding to `subset`, in order to first create the files.
+        path (str, optional): _description_. Defaults to "data/CUB_processed/class_attr_data_10/".
+        resol (int, optional): The expected input height and width by the model,
+            which the transfroms transforms to. Defaults to 224.
+        normalization (str, optional): The normalization method. Defaults to "[-1, 1]".
+        brigthness (float): The brigthness to randomly increase or decreause the
+            training image with, in jittering. Use 0 for no jitter.
+        saturation (list): The interval of possible saturations to randomly jitter
+            image to. Use [1, 1] for no saturation.
+        sampler (callable, optional): Costum sampler. If None, will use standard
+            sampler. Defaults to None.
+        batch_size (int, optional): Batch size to use. Defaults to 4.
+        shuffle (bool, optional): Determines wether the sampler will shuffle or not.
+            It is recommended to use `True` for training and `False` for validating.
+        drop_last (bool, optional): Determines wether the last iteration of an epoch
+            is dropped when the amount of elements is less than `batch_size`.
+
+    Returns:
+        Dataloader: The dataloader, or the list of the three dataloaders.
+    """
+    if mode.lower() == "all":
+        modes = ["train", "val", "test"]
+    else:
+        modes = [mode]
+
+    dataloaders = []
+    mapping = None
+    if subset is not None:  # Map class indices to labels
+        mapping_path = path + str(subset) + "_class_mapping.pkl"
+        mapping = pickle.load(open(mapping_path, "rb"))
+
+    for mode in modes:  # Loop over the train, val, test (or just one of them)
+        transform = get_transforms(mode, resol=resol, normalization=normalization,
+                                   brightness=brightness, saturation=saturation)
+        if subset is not None:
+            full_path = path + str(subset) + "_" + mode + ".pkl"
+        else:
+            full_path = path + mode + ".pkl"
+        dataset = CUBDataset(data_path=full_path, transform=transform, mapping=mapping)
+        dataloader = make_dataloader(dataset, sampler, batch_size, shuffle, drop_last)
+        dataloaders.append(dataloader)
+
+    if len(dataloaders) == 1:  # Just return the datalaoder, not list
+        return dataloaders[0]
+    return dataloaders  # List of (train, val, test) dataloaders
