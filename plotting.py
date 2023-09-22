c01c3d6d3457c06629f2102895a3baf43bf6876b
Tobias <tobiasop@gmail.com>
2023-09-22 19:07:44 +0200

    add history and test-accuracy plotting function.
    Add function in  to plot history and test-accuracy over different subsets of the training data.

--- plotting.py
+++ plotting.py
@@ -146,3 +146,104 @@ def plot_images_worst(image_paths, logits, labels,
     labels = labels[indices[:n_images]]
     preds = logits.argmax(axis=1)
     plot_images_random(wrong_paths, preds, labels, show, n_images, n_cols, title)
+
+
+def plot_training_histories(histories, names, colors=None, attributes=False, title=None,
+                            save_dir="plots/", save_name="test.png"):
+    """
+    Plots training and validation loss and accuracy, in four subplots.
+    Hisotries can be a list of many training histories, which will results in the history being
+    plotted together, so one can easily compare.
+
+    Args:
+        histories (list of dict): List of training histories, as returned or saved by the train functions
+            in `train.py`.
+        names (list of str): List of the names of the models. Must be of same length as histories.
+        colors (list of str, optional): List of colors to use for the different models. Defaults to None.
+        attributes (bool, optional): If True, will plot the attribute stats. Must be a concept-model.
+            Defaults to False.
+        title (str, optional): Title for the plot. Defaults to None.
+        save_dir (str, optional): Path to where the plot will be saved. Defaults to "plots/".
+        save_name (str, optional): Name of saved figure. Defaults to "test.png".
+    """
+    fig = plt.figure()
+    fig.suptitle(title)  # Main title
+    n_hist = len(histories)
+    if colors is None:  # Matplotlib will chose colors for us
+        colors = [None] * n_hist
+
+    if attributes:  # Set name for either class or attributes
+        mid_name = "attr"
+    else:
+        mid_name = "class"
+
+    # Initialize the four subplots
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax1.set_title("Train Loss Over Epochs")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax2.set_title("Validation Loss Over Epochs")
+    ax2.set_xlabel("Epochs")
+    ax2.set_ylabel("Loss")
+    ax3 = fig.add_subplot(2, 2, 3)
+    ax3.set_title("Train Accuracy Over Epochs")
+    ax3.set_xlabel("Epochs")
+    ax3.set_ylabel("Accuracy")
+    ax4 = fig.add_subplot(2, 2, 4)
+    ax4.set_title("Validation Accuracy Over Epochs")
+    ax4.set_xlabel("Epochs")
+    ax4.set_ylabel("Accuracy")
+    axes = [ax1, ax2, ax3, ax4]
+
+    for i in range(n_hist):  # Loop over histories and plot
+        history = histories[i]
+        name = names[i]
+        color = colors[i]
+        axes[0].plot(history["train_" + mid_name + "_loss"], label=f"{name}", c=color)
+        axes[1].plot(history["val_" + mid_name + "_loss"], label=f"{name}", c=color)
+        axes[2].plot(history["train_" + mid_name + "_accuracy"], label=f"{name}", c=color)
+        axes[3].plot(history["val_" + mid_name + "_accuracy"], label=f"{name}", c=color)
+
+    for ax in axes:
+        ax.legend()
+
+    plt.tight_layout()  # To avoid overlap
+    plt.savefig(save_dir + save_name)
+    plt.close()
+
+
+def plot_subset_test_accuracies(x_values, test_accuracies_lists, names, colors=None, title=None,
+                                save_dir="plots/", save_name="test.png"):
+    """
+    Plots the test set accuracy of different models run on different subset sizes.
+    Will plot the test-set accuracy against the amount of training data.
+
+    Args:
+        x_values (list of int): Amount of data used (instances per class).
+        test_accuracies_lists (list of list): List of the models accuracy lists (respective to `x_values`).
+            Each list consist of one models test-accuracies.
+        names (list of str): List of the names of the models
+        colors (list of str, optional): List of colors to use for the different models. Defaults to None.
+        title (str, optional): Title for the plot. Defaults to None.
+        save_dir (str, optional): Path to where the plot will be saved. Defaults to "plots/".
+        save_name (str, optional): Name of saved figure. Defaults to "test.png".
+    """
+    n_models = len(test_accuracies_lists)
+    fig = plt.figure()
+    fig.suptitle(title)
+    if colors is None:
+        colors = [None] * n_models
+
+    for i in range(n_models):
+        error_list = test_accuracies_lists[i]
+        name = names[i]
+        color = colors[i]
+        plt.plot(x_values, error_list, label=name, c=color)  # Lines
+        plt.scatter(x_values, error_list, marker="s", c=color)  # Squares at each point
+
+    plt.legend()
+    plt.xlabel("Subset of each class")
+    plt.ylabel("Accuracy")
+    plt.savefig(save_dir + save_name)
+    plt.close()
