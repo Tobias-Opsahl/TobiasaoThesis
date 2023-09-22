690bc91bc568d0d3cca4d594759684339ef9e882
Tobias <tobiasop@gmail.com>
2023-09-22 19:18:46 +0200

    add evluation and plotting function
    Add functions for evaluation systematically for many subsets and all models. Plots both training hsitory and test-accuracy. Also update run_models.py to be runnable with the new changes.

--- run_shapes.py
+++ run_shapes.py
@@ -5,21 +5,39 @@ import argparse
 import torch
 import torch.nn as nn
 import numpy as np
-from train import train_simple, train_cbm
-from shapes.datasets_shapes import load_data_shapes
-from shapes.models_shapes import ShapesCNN, ShapesCBM, ShapesCBMWithResidual, ShapesCBMWithSkip
 from utils import seed_everything
+from hyperparameter_optimization import run_hyperparameter_optimization_all_models
+from evaluation import run_models_on_subsets_and_plot
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Script for parsing command-line arguments.")
 
+    parser.add_argument("--run_hyperparameters", action="store_true", help="Run hyperparameter search.")
+    parser.add_argument("--evaluate_and_plot", action="store_true", help="Evaluate models and plot.")
+
+    # Path stuff
     parser.add_argument("--dataset_folder", type=str, default="shapes_testing/", help="Name of dataset.")
     parser.add_argument("--base_dir", type=str, default="data/shapes/", help="Path to where dataset is.")
     parser.add_argument("--subset_dir", type=str, default="", help="Name of optional subset.")
+
+    # Parameters
     parser.add_argument("--batch_size", type=int, default=16, help="Batch size for training.")
     parser.add_argument("--n_classes", type=int, default=4, help="Number of classes.")
-    parser.add_argument("--n_attr", type=int, default=7, help="Number of attributes.")
+    parser.add_argument("--n_attr", type=int, default=5, help="Number of attributes.")
+    parser.add_argument("--n_bootstrap", type=int, default=1, help="Number of bootstrap iterations.")
+    parser.add_argument("--n_trials", type=int, default=100, help="Number of trials for hyperparameter search.")
+    parser.add_argument("--fast", action="store_true", help="Use fast testing hyperparameters.")
+
+    # Hardware and div stuff:
+    parser.add_argument("--device", type=str, default=None, help="Device to train on. cuda:0 or CPU.")
+    parser.add_argument("--non_blocking", action="store_true", help="Allows asynchronous RAM to VRAM operations.")
+    parser.add_argument("--num_workers", type=int, default=0, help="Amount of workers to load data to RAM.")
+    parser.add_argument("--pin_memory", action="store_true", help="Pins the RAM memory (makes it non-pagable).")
+    parser.add_argument("--persistent_workers", action="store_true", help="Do not reload workers between epochs")
+    parser.add_argument("--verbose", type=int, default=2, help="Verbosity of training (2, 1 or 0).")
+
+    # Old stuff:
     parser.add_argument("--n_epochs", type=int, default=15, help="Number of epochs for training.")
     parser.add_argument("--n_hidden", type=int, default=16, help="Number of hidden units in cbm skip connection.")
     parser.add_argument("--n_linear_output", type=int, default=64, help="Number of nodes in first linear layer")
@@ -28,11 +46,6 @@ def parse_arguments():
     parser.add_argument("--relu", action="store_true", help="Use relu activation in concept layer.")
     parser.add_argument("--two_layers", action="store_true", help="Use two layers in CBM model after bottleneck.")
     parser.add_argument("--small", action="store_true", help="Use a small model. If not specified, defaults to False.")
-    parser.add_argument("--device", type=str, default=None, help="Device to train on. cuda:0 or CPU.")
-    parser.add_argument("--non_blocking", action="store_true", help="Allows asynchronous RAM to VRAM operations.")
-    parser.add_argument("--num_workers", type=int, default=0, help="Amount of workers to load data to RAM.")
-    parser.add_argument("--pin_memory", action="store_true", help="Pins the RAM memory (makes it non-pagable).")
-    parser.add_argument("--persistent_workers", action="store_true", help="Do not reload workers between epochs")
 
     args = parser.parse_args()
     return args
@@ -42,42 +55,29 @@ if __name__ == "__main__":
     seed_everything(57)
     args = parse_arguments()
 
+    device = args.device
     if args.device is None or args.device == "":
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     path = args.base_dir + args.dataset_folder
-    train_loader, val_loader, test_loader = load_data_shapes(path=path,
-                                                             subset_dir=args.subset_dir,
-                                                             batch_size=args.batch_size,
-                                                             drop_last=True,
-                                                             num_workers=args.num_workers,
-                                                             pin_memory=args.pin_memory,
-                                                             persistent_workers=args.persistent_workers)
-    # Make models
-    cnn = ShapesCNN(n_classes=args.n_classes, n_linear_output=args.n_linear_output)
-    cbm = ShapesCBM(n_classes=args.n_classes, n_attr=args.n_attr, n_linear_output=args.n_linear_output,
-                    use_sigmoid=args.sigmoid, use_relu=args.relu, two_layers=args.two_layers)
-    cbm_res = ShapesCBMWithResidual(n_classes=args.n_classes, n_attr=args.n_attr, n_linear_output=args.n_linear_output,
-                                    use_sigmoid=args.sigmoid, use_relu=args.relu)
-    cbm_skip = ShapesCBMWithSkip(n_classes=args.n_classes, n_attr=args.n_attr, n_hidden=args.n_hidden,
-                                 n_linear_output=args.n_linear_output, use_sigmoid=args.sigmoid, use_relu=args.relu)
+    subsets = [50, 100]  # , 150, 200, 250]
 
-    criterion = nn.CrossEntropyLoss()
-    attr_criterion = nn.BCEWithLogitsLoss()
-    models = [cnn, cbm, cbm_res, cbm_skip]
-
-    # Loop over models and train
-    for model in models:
-        print(f"Running model {model.name}:")
-        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
-        exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.2)
-        if model.name == "ShapesCNN":
-            model = train_simple(model, criterion, optimizer, train_loader, val_loader, scheduler=exp_lr_scheduler,
-                                 n_epochs=args.n_epochs, device=device, non_blocking=args.non_blocking)
+    if args.run_hyperparameters:
+        if args.fast:
+            min_epochs = 2
+            max_epochs = 3
+            base_dir = "hyperparameters/shapes/testing/"
         else:
-            model = train_cbm(model, criterion, attr_criterion, optimizer, train_loader, val_loader,
-                              n_epochs=args.n_epochs, attr_weight=args.attr_weight, scheduler=exp_lr_scheduler,
-                              device=device, non_blocking=args.non_blocking)
-
-    from IPython import embed
-    embed()
+            min_epochs = 10
+            max_epochs = 50
+            base_dir = "hyperparameters/shapes/"
+        run_hyperparameter_optimization_all_models(
+            path, args.n_classes, args.n_attr, n_trials=args.n_trials, base_dir=base_dir, subsets=subsets,
+            batch_size=args.batch_size, eval_loss=True, device=device,
+            num_workers=args.num_workers, pin_memory=args.pin_memory, persistent_workers=args.persistent_workers,
+            non_blocking=args.non_blocking, min_epochs=min_epochs, max_epochs=max_epochs, verbose="info")
+    if args.evaluate_and_plot:
+        run_models_on_subsets_and_plot(
+            path, args.n_classes, args.n_attr, subsets=subsets, n_bootstrap=args.n_bootstrap,
+            fast=args.fast, batch_size=args.batch_size, non_blocking=args.non_blocking, num_workers=args.num_workers,
+            pin_memory=args.pin_memory, persistent_workers=args.persistent_workers, verbose=args.verbose)
